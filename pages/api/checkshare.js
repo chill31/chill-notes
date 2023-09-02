370db5bd270bec9860add7ad3887f98b208cbb2c
Satindar <ibrahimasif711@outlook.com>
2023-09-02 12:07:21 +0530

    fix share issue

--- pages/api/checkshare.js
+++ pages/api/checkshare.js
@@ -1,5 +1,5 @@
 export const config = {
-  runtime: 'edge',
+  runtime: "edge",
 };
 
 import { PrismaClient } from "@prisma/client";
@@ -7,20 +7,23 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const body = JSON.parse(req.body);
-
-  try {
-    const check = await prisma.note.findUnique({
-      where: {
-        uuid: body.uuid
+  if (req.method === "POST") {
+    try {
+      const check = await prisma.note.findUnique({
+        where: {
+          uuid: body.uuid,
+        },
+      });
+      if (check === null) {
+        return res.status(400).json({ msg: "failed", code: 400 });
+      } else {
+        return res.status(200).json({ msg: JSON.stringify(check), code: 200 });
       }
-    });
-    if (check === null) {
-      res.status(400).json({ msg: 'failed', code: 400 });
-    } else {
-      res.status(200).json({ msg: JSON.stringify(check), code: 200 });
+    } catch (e) {
+      return res.status(400).json({ msg: "failed", code: 400 });
     }
-  } catch (e) {
-    res.status(400).json({ msg: 'failed', code: 400 })
   }
-
+  else {
+    return res.status(200).json({ msg: "Only POST method allowed", code: 405 });
+  }
 }
--- pages/api/share.js
+++ pages/api/share.js
@@ -7,11 +7,18 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
 
+  if(req.method === "POST") {
+      
   const recievedData = JSON.parse(req.body);
   const createdNote = await prisma.note.create({
     data: recievedData
   });
 
 
-  res.status(200).json({ uuid: recievedData.uuid });
+  return res.status(200).json({ uuid: createdNote.uuid });
+  }
+  else{
+    return res.status(200).json({ msg: "Only POST method allowed", code: 405 });
+  }
+
 }
